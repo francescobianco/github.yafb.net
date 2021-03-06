ecba4c7424fba98228d854a7338f5fb68e4c3a6d
francescobianco <info.francescobianco@gmail.com>
2021-03-06 22:54:53 +0100

    introducing files structure

--- bin/build.sh
+++ bin/build.sh
@@ -3,8 +3,11 @@
 wget -kO docs/index.html https://github.com/francescobianco
 csplit docs/index.html /\<\/head\>/
 cat xx00 etc/analytics.html xx01 > docs/index.html
+csplit docs/index.html /\<\/body\>/
+cat xx00 etc/ribbon.html xx01 > docs/index.html
 sed -i '/^[[:space:]]*$/d' docs/index.html
 sed -i 's/^[ \t]*//' docs/index.html
 sed -i 's/\<form /form style="display:none" /' docs/index.html
 sed -i 's/class="HeaderMenu\-link flex\-shrink\-0 /style="display:none" ignore-class="/' docs/index.html
+
 rm xx00 xx01
