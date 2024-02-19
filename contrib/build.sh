5479697405bc81960b47724bf7a9a0d18f07905f
francesco <info.francescobianco@gmail.com>
2024-02-19 18:10:32 +0100

    fix confetti

--- contrib/build.sh
+++ contrib/build.sh
@@ -2,9 +2,9 @@
 set -e
 
 wget -kO index.html https://github.com/francescobianco
-csplit index.html /\<\/head\>/
-cat xx00 etc/analytics.html xx01 > index.html
-csplit index.html /\<\/body\>/
+csplit index.html /\</head\>/
+cat xx00 include/analytics.html xx01 > index.html
+csplit index.html /\</body\>/
 cat xx00 include/ribbon.html xx01 > index.html
 sed -i '/^[[:space:]]*$/d' index.html
 sed -i 's/^[ \t]*//' index.html
